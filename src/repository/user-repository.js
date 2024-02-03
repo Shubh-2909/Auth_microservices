b52d56e35768bc2d3bedfd32c5caa03db380deb4
Shubh Shubhanjal <shubhshubhanjal96@gmail.com>
2024-02-03 16:55:37 +0530

    Added get user by id in repo

--- src/repository/user-repository.js
+++ src/repository/user-repository.js
@@ -24,6 +24,19 @@ class UserRepository{
         }
     }
 
+    async getById(userId){
+        try {
+            const user = await User.findByPk(userId , {
+                attributes : ['email' , 'id']
+            });
+            // attributes are used to get some particular data not all the data . in this case we want [email , id] only not password. In order to get all just write findByPk(userId) , nothing to pass in attributes.
+            return user;
+        } catch (error) {
+            console.log("Something went wrong on repository layer");
+            throw error;
+        }
+    }    
+
 }
 
 module.exports=UserRepository;
